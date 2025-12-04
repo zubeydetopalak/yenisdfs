fb8f9d6c7f95bfab10fc35b4a7991c99380d0cbc
Zubeyde Nur Topalak <165080076+zubeydetopalak@users.noreply.github.com>
2025-12-04 11:00:12 +0300

    Add comment for new addition in TodoServiceTest
    Added a comment indicating a new addition.

--- src/test/java/com/test/app/service/TodoServiceTest.java
+++ src/test/java/com/test/app/service/TodoServiceTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class TodoServiceTest {
+    // yeni eklendi
     @Mock
     private TodoRepository todoRepository;
 
